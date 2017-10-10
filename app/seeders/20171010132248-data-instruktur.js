e2e53a92cc676d20028eefbaff028aae0cdae8ce
setiadijoe <setiadi_y56@yahoo.co.id>
2017-10-10 20:27:33 +0700

    model instructor dan seeder

---
+++ app/seeders/20171010132248-data-instruktur.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Instructors', [{
+      name: 'Irsyah Mardiah'
+    },{
+      name: 'Ryan Akbar'
+    },{
+      name: 'Windiana Chrismanuyar'
+    },{
+      name: 'Adithya Hidayah'
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};

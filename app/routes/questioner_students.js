2579c1e8a95e9edbbafbce515e8e915edd070e29
setiadijoe <setiadi_y56@yahoo.co.id>
2017-10-11 17:16:32 +0700

    data tabel sudah bisa masukin data dari body

--- app/routes/questioner_students.js
+++ app/routes/questioner_students.js
@@ -2,55 +2,62 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models');
 
-router.get('/', (req, res)=>{
-  model.Questioner.findAll({order:[['id', 'ASC']]})
-  .then((dataQuestioner)=>{
-    model.User.findAll()
-      .then((dataUser)=>{
-        res.render('questioner_students', {dataUser: dataUser, dataQuestioner : dataQuestioner})
-      })
+router.use((req, res, next) => {
+  if (req.session.hasLogin) {
+    next()
+  } else {
+    res.redirect('/')
+  }
+})
+
+router.get('/', (req, res) => {
+  model.Instructor.findAll()
+    .then(instructor => {
+      res.render('answer-questioner/select-instructor', { data: instructor, title: 'Select', session: req.session })
     })
 })
 
 
-router.get('/:id/answer', (req, res)=>{
-  model.UserQuestioner.findAll({
-    where: {
-      UserId: req.params.id
-    },
-    include: [{all: true}]
-  })
-  .then(dataUserQuestioner=>{
-    model.Questioner.findAll()
-    .then(dataQuestioner=>{
-      res.send({dataUserQuestioner, dataQuestioner})
-    })
-
-    // res.render('questioner_students', {dataUserQuestioner: dataUserQuestioner})
-  })
-  .catch(err=>{
-    res.send(err)
-  })
+router.post('/', (req, res) => {
+  // req.session.user = {
+  //   instructors: req.body.nama_instruktur
+  // }
+  // console.log('====================session===================', req.session);
+  // console.log('---------------------body---------------------', req.body);
+  // model.Instructor.findById(req.params.id)
+  res.redirect(`/questioner_student/${req.body.id}/answer`)
 })
 
-router.post('/:id/answer', (req, res)=>{
-  console.log(req.body);
-  for (var i = 0 ; i< 14; i++){
+router.get('/:id/answer', (req, res) => {
+  model.Questioner.findAll({ order: [['id', 'ASC']] })
+    .then((dataQuestioner) => {
+      model.User.findAll()
+        .then((dataUser) => {
+          res.render('answer-questioner/questioner_students', { dataUser: dataUser, dataQuestioner: dataQuestioner, session: req.session, title: 'Quest' })
+        })
+    })
+})
+
+router.post('/:id/answer', (req, res) => {
+  // console.log(req.session);
+  for (var i = 0; i < 13; i++) {
     model.UserQuestioner.create({
-      UserId:req.params.id,
+      UserId: req.session.user.id,
       QuestionerId: req.body.QuestionerId[i],
       nilai_harapan: req.body.nilai_harapan[i],
       nilai_kenyataan: req.body.nilai_kenyataan[i],
+      InstructorId: req.params.id,
       createdAt: new Date(),
       updatedAt: new Date()
     })
-    .then((ok)=>{
-      // res.send(ok)
-      res.send('Anda Telah mengisi kuesioner')
-    })
-    .catch(err=>{
-      res.send(err)
-    })
+      .then(() => {
+        // res.send(ok)
+        // res.send('Anda Telah mengisi kuesioner')
+        res.redirect('/questioner_students')
+      })
+      .catch(err => {
+        res.send(err)
+      })
   }
 })
 
